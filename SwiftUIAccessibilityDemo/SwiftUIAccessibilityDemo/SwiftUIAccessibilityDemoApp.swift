88eb2802e3ee81c5ecb7f2f882dd45332bbd80d8
Suni <hyunsun819@gmail.com>
2022-09-22 20:58:53 +0900

    Create

---
+++ SwiftUIAccessibilityDemo/SwiftUIAccessibilityDemo/SwiftUIAccessibilityDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAccessibilityDemoApp.swift
+//  SwiftUIAccessibilityDemo
+//
+//  Created by suni on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAccessibilityDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8da8ba694ea5990c8ce0b9cfd94f7ba1b5f4f526
Suni <hyunsun819@gmail.com>
2022-09-23 14:55:06 +0900

    feat : 뮤직 플레이어 뷰, 백버튼 완성

--- SwiftUIAccessibilityDemo/SwiftUIAccessibilityDemo/Views/MusicPlayer.swift
+++ SwiftUIAccessibilityDemo/SwiftUIAccessibilityDemo/Views/MusicPlayer.swift
@@ -6,15 +6,57 @@
 //
 
 import SwiftUI
-
-
+import AVKit
 struct MusicPlayer: View {
+    @State var audioPlayer: AVAudioPlayer!
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text("뮤직플레이어")
+        ZStack {
+            ZStack {
+                Button {
+                    self.presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .imageScale(.large)
+                        .padding()
+                }
+            }
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: Alignment.topLeading
+            )
+            VStack {
+                Text("Play").font(.system(size: 45)).font(.largeTitle)
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.audioPlayer.play()
+                    }) {
+                        Image(systemName: "play.circle.fill").resizable()
+                            .frame(width: 50, height: 50)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    Spacer()
+                    Button(action: {
+                        self.audioPlayer.pause()
+                    }) {
+                        Image(systemName: "pause.circle.fill").resizable()
+                            .frame(width: 50, height: 50)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    Spacer()
+                }
+            }
+        }
+        .onAppear {
+            let sound = Bundle.main.path(forResource: "song", ofType: "mp3")
+            self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        }
         .navigationBarHidden(true)
     }
 }
-
 struct MusicPlayer_Previews: PreviewProvider {
     static var previews: some View {
         MusicPlayer()
